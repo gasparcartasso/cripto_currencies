1842b45d17007c1407209668874ea961709b02dd
Gaspar Cartasso <gasparcartasso@Gaspars-MacBook-Air.local>
2025-10-19 20:25:21 +1100

    datafarmes

--- API_request.py
+++ API_request.py
@@ -1,7 +1,9 @@
+#%%
 import requests
 def get_bitcoin_price():
     import requests
     import datetime as dt
+    import pandas as pd
     date = dt.date.today().strftime("%d-%m-%Y")
     url = "https://api.coingecko.com/api/v3/coins/bitcoin/history"
     params = {
@@ -11,9 +13,11 @@ def get_bitcoin_price():
 
     response = requests.get(url, params=params)
     data = response.json()
-    return data['market_data']['current_price']['usd']
-
-def get_bitcoin_price_2decimals():
+    df_price = pd.DataFrame([data['market_data']['current_price']])
+    df_price['date']=date
+    return df_price
+#%%
+def get_bitcoin_price_2decimals_usd():
     import requests
     import datetime as dt
     date = dt.date.today().strftime("%d-%m-%Y")
