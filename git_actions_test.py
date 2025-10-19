5828ca774afcf014444c309052842cf59eb16ef7
Gaspar Cartasso <gasparcartasso@Gaspars-MacBook-Air.local>
2025-10-19 20:29:22 +1100

    error git action fixed

--- git_actions_test.py
+++ git_actions_test.py
@@ -1,7 +1,7 @@
 from unittest.mock import patch
-from API_request import get_bitcoin_price_2decimals
+from API_request import get_bitcoin_price_2decimals_usd
 
 @patch("API_request.requests.get")
 def test_get_bitcoin_price(mock_get):
     mock_get.return_value.json.return_value = {'market_data':{'current_price':{'usd':1.54563}}}
-    assert isinstance(get_bitcoin_price_2decimals(),float)
+    assert isinstance(get_bitcoin_price_2decimals_usd(),float)
