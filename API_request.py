c3ca79e565752f5b631102bbfc70fa49824baacd
Gaspar Cartasso <gasparcartasso@Gaspars-MacBook-Air.local>
2025-10-26 14:15:02 +1100

    updated

--- API_request.py
+++ API_request.py
@@ -1,9 +1,8 @@
 #%%
 import requests
+import datetime as dt
+import pandas as pd
 def get_bitcoin_price():
-    import requests
-    import datetime as dt
-    import pandas as pd
     date = dt.date.today().strftime("%d-%m-%Y")
     url = "https://api.coingecko.com/api/v3/coins/bitcoin/history"
     params = {
@@ -18,8 +17,6 @@ def get_bitcoin_price():
     return df_price
 #%%
 def get_bitcoin_price_2decimals_usd():
-    import requests
-    import datetime as dt
     date = dt.date.today().strftime("%d-%m-%Y")
     url = "https://api.coingecko.com/api/v3/coins/bitcoin/history"
     params = {
@@ -30,3 +27,59 @@ def get_bitcoin_price_2decimals_usd():
     response = requests.get(url, params=params)
     data = response.json()
     return round(data['market_data']['current_price']['usd'],2)
+
+def get_prices():
+    """
+    Gets the prices of the relevant cripto currencies of a certain date and returns their values.
+    Returns (pd.DataFrame)
+    """
+    date = dt.date.today().strftime("%d-%m-%Y")
+    currencies=['bitcoin','ethereum','tether','solana']
+    df_price = pd.DataFrame()
+    for currency in currencies:
+        url = f"https://api.coingecko.com/api/v3/coins/{currency}/history"
+        params = {
+        "date": f"{date}",  # Format: DD-MM-YYYY
+        "localization": "false"
+        }
+        response = requests.get(url, params=params)
+        data = response.json()
+        df_price = pd.concat([df_price,pd.DataFrame({'usd':data['market_data']['current_price']['usd'],'cripto':[currency]})],ignore_index=True)
+    df_price['date']=date
+    return df_price
+
+def get_max_52w_rolling(history):
+    """
+    This function gets a 52 week rolling max of the currency
+    Arg (pd.DataFrame): history to analyse
+    Return (pd.DataFrame): max by date of each cripto currency
+    """
+    currencies = history['cripto'].unique()
+    price_max=pd.DataFrame()
+    for currency in currencies:
+        price_max = pd.concat([price_max,pd.DataFrame({'usd_max_year':history[history['cripto']==currency].sort_values(by='date',ascending=True)['usd'].rolling(window=365,min_periods=1).max(),'cripto':history[history['cripto']==currency].sort_values(by='date',ascending=True)['cripto'],'date':history[history['cripto']==currency].sort_values(by='date',ascending=True)['date']})],ignore_index=True)
+    return price_max
+
+def get_avg_52w_rolling(history):
+    """
+    This function gets a 52 week rolling avg of the currency
+    Arg (pd.DataFrame): history to analyse
+    Return (pd.DataFrame): avg by date of each cripto currency
+    """
+    currencies = history['cripto'].unique()
+    price_avg=pd.DataFrame()
+    for currency in currencies:
+        price_avg = pd.concat([price_avg,pd.DataFrame({'usd_avg_year':history[history['cripto']==currency].sort_values(by='date',ascending=True)['usd'].rolling(window=365,min_periods=1).mean(),'cripto':history[history['cripto']==currency].sort_values(by='date',ascending=True)['cripto'],'date':history[history['cripto']==currency].sort_values(by='date',ascending=True)['date']})],ignore_index=True)
+    return price_avg
+
+def get_min_52w_rolling(history):
+    """
+    This function gets a 52 week rolling avg of the currency
+    Arg (pd.DataFrame): history to analyse
+    Return (pd.DataFrame): avg by date of each cripto currency
+    """
+    currencies = history['cripto'].unique()
+    price_min=pd.DataFrame()
+    for currency in currencies:
+        price_min = pd.concat([price_min,pd.DataFrame({'usd_min_year':history[history['cripto']==currency].sort_values(by='date',ascending=True)['usd'].rolling(window=365,min_periods=1).min(),'cripto':history[history['cripto']==currency].sort_values(by='date',ascending=True)['cripto'],'date':history[history['cripto']==currency].sort_values(by='date',ascending=True)['date']})],ignore_index=True)
+    return price_min
--- git_actions_test.py
+++ git_actions_test.py
@@ -1,7 +1,13 @@
 from unittest.mock import patch
-from API_request import get_bitcoin_price_2decimals_usd
+import pandas as pd
+from API_request import get_bitcoin_price_2decimals_usd, get_max_52w_rolling
 
 @patch("API_request.requests.get")
 def test_get_bitcoin_price(mock_get):
     mock_get.return_value.json.return_value = {'market_data':{'current_price':{'usd':1.54563}}}
     assert isinstance(get_bitcoin_price_2decimals_usd(),float)
+
+def test_get_max_52_week_rolling():
+    history = pd.DataFrame({'usd':[1.352,1.312542463,21414.124,1243],'cripto':['bitcoin','bitcoin','bitcoin','bitcoin'],'date':['23-10-2025','24-10-2025','25-10-2025','26-10-2025']})
+    result = get_max_52w_rolling(history)
+    assert result['usd_max_year'].max==1243
